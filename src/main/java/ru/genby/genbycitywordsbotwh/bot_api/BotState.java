7529116cab54c5951bffdf9a9dcb3c22955b737f
Kirillputin <WhirlPool1488>
2023-03-15 13:55:19 +0500

    Version 1.00

--- src/main/java/ru/genby/genbycitywordsbotwh/bot_api/BotState.java
+++ src/main/java/ru/genby/genbycitywordsbotwh/bot_api/BotState.java
@@ -1,15 +1,15 @@
 package ru.genby.genbycitywordsbotwh.bot_api;
 
 public enum BotState {
-    GAME,
-    START,
-    END,
-    SCOPE,
-    END_GAME,
-    START_GAME,
-    ASK_NAME,
-    ASK_END,
-    ANS_END,
-    FILLING_PROFILE,
-    PROFILE_FILLED
+    GAME, // Процесс игры
+    START, //Старт обработки
+    END, //Полное окончание обработки
+    SCOPE,//Таблица с лидерами
+    END_GAME,//Окончание игрового процесса
+    START_GAME,//Начало игры, первый город
+    ASK_NAME,//Заполнение профиля, спрашиваем имя
+    ASK_END,//Спрашиваем об окончании игры
+    ANS_END,//Обрабатываем ответ от пользователя об окончании игры
+    FILLING_PROFILE,//Заполнение профиля, начало
+    PROFILE_FILLED //Профиль заполнен
 }
